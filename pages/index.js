e1601ccc5d85c4ce8c3753cf94fece7e7866b1be
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-03-02 21:18:24 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,34 @@ import styles from '../styles/VotePage.module.css'
 import { useState } from 'react'
 import { SHA256 } from 'crypto-js'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   const [candidate, setCandidate] = useState('');
-  const [hashGerado, setHash] = useState('');
+  const [eleicao, setEleicao] = useState('');
+  const [hashEleicao, setHashEleicao] = useState('');
+  const [hashVoto, setHashVoto] = useState('');
+  
 
   const handleCandidateChange = (event) => {
     setCandidate(event.target.value);
   };
+  const handleEleicaoChange = (event) => {
+    setEleicao(event.target.value);
+  };
+
+  const generateHash=(text)=> {
+    return SHA256(text).toString();
+  }
+
 
   const handleVote = (e) => {
-    e.eventPrevent()
+    e.preventDefault();
     //chamar da API do Helios para registrar o voto do usuário
-    console.log(`Votou em ${candidate}`);
+    const candidateString = JSON.stringify(candidate);
+    const eleicaoString = JSON.stringify(eleicao);
+    setHashEleicao(generateHash(candidateString))  
+    setHashVoto(generateHash(eleicaoString))
+    console.log(`hash armazenado do voto: ${hashVoto}`);
+    console.log(`hash armazenado da eleição: ${hashEleicao}`);
   };
 
   return (
@@ -27,17 +41,24 @@ export default function Home() {
       <p className={styles.subtitle}>Escolha seu candidato:</p>
 
       <form onSubmit={handleVote}>
+        <label className={styles.fontLabel}>Nome da eleição</label><br />
+        <input className={styles.inputs} type="text" name="eleicao" onChange={handleEleicaoChange} value={eleicao} />
+
         <label className={styles.label}>
           <input className={styles.input} type="radio" name="candidate" value="Candidato A" onChange={handleCandidateChange} /> Candidato A
         </label>
         <label className={styles.label}>
-          <input className={styles.input} type="radio" name="candidato B" value="Candidato B" onChange={handleCandidateChange} /> Candidato B
+          <input className={styles.input} type="radio" name="candidate" value="Candidato B" onChange={handleCandidateChange} /> Candidato B
         </label>
         <label className={styles.label}>
           <input className={styles.input} type="radio" name="candidate" value="Candidato C" onChange={handleCandidateChange} /> Candidato C
         </label>
-        <label className={styles.fontLabel}>hash gerado</label><br/>
-        <input className={styles.inputs} style={{ textAlign: 'left' }} type="String" name="resultado" onChange={handleCandidateChange} value={hashGerado} disabled /><br/>
+
+        <label className={styles.fontLabel}>hash Eleição</label><br />
+        <input className={styles.inputs} style={{ textAlign: 'left' }} type="String" name="resultado"  value={hashEleicao} disabled /><br />
+
+        <label className={styles.fontLabel}>hash Voto</label><br />
+        <input className={styles.inputs} style={{ textAlign: 'left' }} type="String" name="resultado"  value={hashVoto} disabled /><br />
 
         <button className={styles.voteButton}>Votar</button>
 
